ae35b5b810a6d8bc3688a104d9b6e32b1b84402f
Ashwin Hariharan <hariharan.ashwin44@gmail.com>
2021-07-02 15:42:42 -0400

    finishing updates on index website

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+
+
+
 navToggle.addEventListener('click', () => {
 
 
@@ -11,3 +16,17 @@ navToggle.addEventListener('click', () => {
     
 
 });
+
+
+navLinks.forEach(link => {
+
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+        
+
+
+
+    })
+
+
+})
